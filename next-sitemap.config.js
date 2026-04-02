653c782705738fbc7104e041c23a216566e14053
Operations2 <operations2@completestaffingsolutions.com>
2026-04-02 13:00:12 -0400

    SiteMap Change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://completestaffingsolutions.com",
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://www.completestaffingsolutions.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: "weekly",
